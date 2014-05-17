737cae3e072c83ae66bde29c57f53d33a4ffb4a5
Daniel Knell <contact@danielknell.co.uk>
2014-05-17 22:24:05 +0100

    image generation

--- web.rb
+++ web.rb
@@ -1,9 +1,31 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  "Hello World!"
+end
+
+def image_for size
+  images = Dir.entries(File.dirname(__FILE__) + '/images').reject! { |x| x[0] == '.' }
+
+  File.dirname(__FILE__) + '/images/' + images["#{params[:width]}x#{params[:height]}".hash % images.length]
 end
 
 get '/:width/:height' do
-  "#{params[:width]} x #{params[:height]}"
+  size = "#{params[:width]}x#{params[:height]}"
+
+  thumb = File.dirname(__FILE__) + "/thumbs/#{size}.jpg"
+
+  unless File.exists?(thumb) and (Time.now - File.stat(thumb).mtime <= 3600)
+    image = image_for(size)
+
+    FileUtils.mkdir_p(File.dirname(thumb))
+
+    command = "convert #{image} -thumbnail #{size}^ -gravity center -extent #{size} #{thumb}"
+
+    output = `#{command} 2>&1`
+
+    raise "couldn't run #{command}: #{output}" unless $?.success?
+  end
+
+  send_file thumb, :type => :jpg
 end
